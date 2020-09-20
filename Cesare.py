31d71e1bf92e1c97dbc3dee54092947c4b7d8c65
Pakholyuk-Maria <71166439+Pakholyuk-Maria@users.noreply.github.com>
2020-09-20 19:54:19 +0500

    Update Cesare.py

--- Cesare.py
+++ Cesare.py
@@ -6,8 +6,9 @@ msg = input()
 alphabet = ['А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
 
 for a in list(msg.upper()):
+    if a == 'Я':
+        a = 'A'
     if a in alphabet:
-        a = a.upper()
         n = alphabet.index(a)+1
         new_msg += alphabet[n]
     else:
